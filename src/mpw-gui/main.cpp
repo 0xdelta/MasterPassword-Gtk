6e72fc372d996a154287ce038136fca89c3d25f9
delta242 <lukas.zey@gmail.com>
2016-04-14 10:22:10 +0200

    Better error handling for file system access

--- src/mpw-gui/main.cpp
+++ src/mpw-gui/main.cpp
@@ -1,6 +1,7 @@
 #include <gtkmm/application.h>
 #include <user_manager.h>
 #include <gtkmm/messagedialog.h>
+#include <iostream>
 #include "mpw_login_window.h"
 #include "mpw_password_window.h"
 
@@ -9,17 +10,16 @@ std::string getResourceDir() {
 }
 
 int main(int argc, char *argv[]) {
+    // Init gtk (this has to be done before any window is shown)
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
+
     UserManager userManager;
     if (!userManager.readFromConfig()) {
         Gtk::MessageDialog dialog("Error", false, Gtk::MESSAGE_ERROR);
         dialog.set_secondary_text("Could not read main config. \n\nSee log for more details.");
         dialog.run();
-
         return 1;
     }
-
-    // Init gtk
-    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv);
 
     // Init the login window
     mpw_login_window *loginWindow = mpw_login_window::create(&userManager);
